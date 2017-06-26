cb825ca8aa95a4255d3cb86121d49d7bd9daa4e7
Nicolas <nicolasthevenon.fr@gmail.com>
2017-06-26 09:57:44 +0800

    Tu SHLING

--- pokemon/new_games/Poke_Dex/javascript.js
+++ pokemon/new_games/Poke_Dex/javascript.js
@@ -10,7 +10,7 @@ $("#Pikachu").click(function() {
 	$("#Mewtwo").css("background-color", "white");
 });
 $("#Kyurem").click(function() {
-	$("#info").text("Kyurem");
+	$("#info").text("");
 	$("#Pikachu").css("background-color", "white");
 	$("#Kyurem").css("background-color", "red");
 	$("#Giratina").css("background-color", "white");
@@ -18,4 +18,46 @@ $("#Kyurem").click(function() {
 	$("#Reshiram").css("background-color", "white");
 	$("#Mewtwo").css("background-color", "white");
 });
+$("#Giratina").click(function() {
+	$("#info").text("Giratina");
+	$("#Pikachu").css("background-color", "white");
+	$("#Kyurem").css("background-color", "white");
+	$("#Giratina").css("background-color", "red");
+	$("#Zekrom").css("background-color", "white");
+	$("#Reshiram").css("background-color", "white");
+	$("#Mewtwo").css("background-color", "white");
 });
+$("#Zekrom").click(function() {
+	$("#info").text("Zekrom");
+	$("#Pikachu").css("background-color", "white");
+	$("#Kyurem").css("background-color", "white");
+	$("#Giratina").css("background-color", "white");
+	$("#Zekrom").css("background-color", "red");
+	$("#Reshiram").css("background-color", "white");
+	$("#Mewtwo").css("background-color", "white");
+});
+$("#Reshiram").click(function() {
+	$("#info").text("Reshiram");
+	$("#Pikachu").css("background-color", "white");
+	$("#Kyurem").css("background-color", "white");
+	$("#Giratina").css("background-color", "white");
+	$("#Zekrom").css("background-color", "white");
+	$("#Reshiram").css("background-color", "red");
+	$("#Mewtwo").css("background-color", "white");
+});
+$("#Mewtwo").click(function() {
+	$("#info").text("Mewtwo");
+	$("#Pikachu").css("background-color", "white");
+	$("#Kyurem").css("background-color", "white");
+	$("#Giratina").css("background-color", "white");
+	$("#Zekrom").css("background-color", "white");
+	$("#Reshiram").css("background-color", "white");
+	$("#Mewtwo").css("background-color", "red");
+});
+});
+
+
+
+
+
+
